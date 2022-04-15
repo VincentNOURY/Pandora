f40e5db631ec7a653f3fa7adbb069da3b98b4511
VincentNOURY <vincent.noury@efrei.net>
2022-04-14 18:36:50 -0700

    Added login

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const { auth, requiresAuth } = require('express-openid-connect')
+
+require('dotenv').config()
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+}
+
+app.use(auth(config))
 
 app.get('/', (req, res) => {
-    res.send('homepage')
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+})
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  let jsonProfile = req.oidc.user
+  res.send(JSON.stringify(jsonProfile))
 })
 
 app.get('/login', (req, res) => {
