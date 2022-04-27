dea170d690963bcbe3e604ead50c9c89184798e0
Pernam75 <jules.rubin@efrei.net>
2022-04-27 12:52:00 -0700

    add message and create thread functions

--- app.js
+++ app.js
@@ -64,3 +64,32 @@ app.get('/new', async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+const fs = require('fs')
+function createThread(threadName) {
+  const threadPath = 'database/threads/'+threadName+'.json'
+  let json = JSON.stringify({name: threadName, path: threadPath});
+  fs.writeFile('database/threads.json', json, function(err, result) {
+    if(err) console.log('error', err);
+  })
+
+  json = ''
+
+  fs.writeFile(threadPath, json, function(err, result) {
+    if(err) console.log('error', err)
+  })
+}
+
+function addMessage(messageContent, author, date, threadName) {
+  const threadPath = 'database/threads/'+threadName+'.json'
+
+  const json = JSON.stringify({author: author, date: date, messageContent: messageContent})
+
+  fs.writeFile(threadPath, json, function(err, result) {
+    if(err) console.log('error', err)
+  })
+}
+//createThread("test2")
+addMessage("This is a test mesage", 'jules', '04/27/2022', 'test2')
+
