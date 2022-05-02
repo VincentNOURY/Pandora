17c2d9cf3d164f90b3d86ea5c4bb874d3ed12a71
VincentNOURY <vincent.noury@efrei.net>
2022-05-01 20:06:47 -0700

    Handled spaces in the thread name

--- app.js
+++ app.js
@@ -35,9 +35,18 @@ app.get('/', async (req, res) => {
   let randomInfos = getRandomThread()
   let tendancyInfos = getTendancyThread()
 
-  let results = {newestLink: '/thread/' + newestInfos, newestName: newestInfos, 
-                 randomLink: '/thread/' + randomInfos, randomName: randomInfos, 
+  let results = {newestLink: '/thread/' + newestInfos, newestName: newestInfos,
+                 randomLink: '/thread/' + randomInfos, randomName: randomInfos,
                  tendancyLink: '/thread/' + tendancyInfos, tendancyName: tendancyInfos}
+  if (typeof newestInfos === "undefined"){
+    results['newestLink'] = "#"
+  }
+  if (typeof randomInfos === "undefined"){
+    results['randomLink'] = "#"
+  }
+  if (typeof tendancyInfos === "undefined"){
+    results['tendancyLink'] = "#"
+  }
   results['authenticated'] = req.oidc.isAuthenticated()
   await res.render('pages/index', results)
 })
@@ -50,9 +59,9 @@ app.get('/profile', requiresAuth(), (req, res) => {
 
 app.get('/newthread', (req, res) =>{
   if (req.oidc.isAuthenticated()){
-    console.log(req.oidc.user)
     if (Object.keys(req.query).length > 0){
       let threadId = req.query.threadid
+      threadId.replace(" ", "%20")
       if (threadExists(threadId)){
         res.render('pages/newThread.ejs', {exists: true, authenticated: req.oidc.isAuthenticated()})
       }
@@ -64,7 +73,7 @@ app.get('/newthread', (req, res) =>{
     else{
       res.render('pages/newThread.ejs', {exists: false, authenticated: req.oidc.isAuthenticated()})
     }
-    
+
   }
   else{
     res.redirect('/login')
@@ -80,12 +89,15 @@ app.get('/thread/:id', async (req, res) => {
   let verif = false
   let name = ""
   let threads = readThreads()
-  Object.keys(threads).forEach(element => {
-    if (threads[element].name.match(id)){
-      verif = true
-      name = threads[element].name
-    }
-  })
+  if (typeof threads !== "undefined"){
+    Object.keys(threads).forEach(element => {
+      if (threads[element].name.match(id)){
+        verif = true
+        name = threads[element].name
+      }
+    })
+  }
+
   if (verif){
     let results = readThread(id)
     await res.render('pages/threadTemplate', {authenticated: req.oidc.isAuthenticated(), title: name, results: results})
@@ -102,13 +114,14 @@ app.get('/threads', async (req, res) => {
 app.get('/search', async (req, res) => {
   let threads = readThreads()
   let results = []
-  Object.keys(threads).forEach(element => {
-    if (threads[element].name.match(req.query.search)){
-      let threadName = threads[element].name
-      results.push({name: threadName, description: readThread(threadName)[0].messageContent, link: "/thread/" + threadName})
-    }
-  }); 
-  
+  if (typeof threads !== "undefined"){
+    Object.keys(threads).forEach(element => {
+      if (threads[element].name.match(req.query.search)){
+        let threadName = threads[element].name
+        results.push({name: threadName, description: readThread(threadName)[0].messageContent, link: "/thread/" + threadName})
+      }
+    })
+  }
   await res.render('pages/search', {results: results, authenticated: req.oidc.isAuthenticated()})
 })
 
@@ -116,8 +129,9 @@ app.get('/new', async (req, res) => {
   if (req.oidc.isAuthenticated()){
     let message = req.query.message
     let threadId = req.query.threadid
+    threadId.replace(" ", "%20")
+    threadId.replace(" ", "%20")
     addMessage(message, req.oidc.user.nickname, threadId)
-    //console.log(Object.keys(req))
     await res.redirect('thread/' + threadId)
   }
   else{
@@ -130,12 +144,20 @@ app.listen(port, () => {
 })
 
 function createThread(threadName, authorName, message) {
+  threadName.replace(" ", "%20")
   const threadPath = `database/threads/${threadName}.json`
   const newThread = {name: threadName, path:threadPath}
 
   fs.readFile('database/threads.json', function (err, data) {
-      let json = JSON.parse(data)
+    let json = ""
+    try{
+      json = JSON.parse(data)
       json.push(newThread)
+    }
+    catch (err){
+      json = [newThread]
+    }
+
 
       fs.writeFile("database/threads.json", JSON.stringify(json), function(err, result) {
         if(err) console.log('error', err)})
@@ -163,11 +185,17 @@ function addMessage(messageContent, author, threadName) {
 }
 
 function readThread(name){
-  const threadPath = `database/threads/${name}.json`
-  try {
-    return JSON.parse(fs.readFileSync(threadPath, 'utf8'))
-  } catch (err) {
-    console.error(err)
+  if (typeof name !== "undefined"){
+    const threadPath = `database/threads/${name}.json`
+    try {
+      return JSON.parse(fs.readFileSync(threadPath, 'utf8'))
+    } catch (err) {
+      console.error(err)
+      return undefined
+    }
+  }
+  else{
+    return undefined
   }
 }
 
@@ -175,47 +203,80 @@ function readThread(name){
 function readThreads(){
   const threadPath = `database/threads.json`
   try {
-    return JSON.parse(fs.readFileSync(threadPath, 'utf8'))
+    let parsed = JSON.parse(fs.readFileSync(threadPath, 'utf8'))
+    if (typeof parsed !== "undefined"){
+      return parsed
+    }
+      return undefined
   } catch (err) {
     console.error(err)
+    return undefined
   }
 }
 
 function getRandomThread(){
   let threads = readThreads()
-  let num = Math.floor(Math.random() * (threads.length))
-  return threads[num].name
+  if (typeof threads !== "undefined"){
+    let num = Math.floor(Math.random() * (threads.length))
+    return threads[num].name
+  }
+  else{
+    return undefined
+  }
 }
 
 function getThreadLength(thread){
-  return readThread(thread.name).length
+  if (Object.keys(thread).length > 0){
+    return readThread(thread.name).length
+  }
+  else{
+    return undefined
+  }
 }
 
 function getTendancyThread(){
   let threads = readThreads()
-  let max = getThreadLength(threads[0])
-  let threadId = threads[0]
-  for (i = 1; i < threads.length; i++){
-    let len = getThreadLength(threads[i])
-    if (len > max){
-      max = len
-      threadId = threads[i]
+  if (typeof threads !== "undefined"){
+    let max = getThreadLength(threads[0])
+    let threadId = threads[0]
+    for (i = 1; i < threads.length; i++){
+      let len = getThreadLength(threads[i])
+      if (len > max){
+        max = len
+        threadId = threads[i]
+      }
+    }
+    if (typeof max !== "undefined"){
+      return threadId.name
+    }
+    else{
+      return undefined
     }
   }
-  return threadId.name
+  else{
+    return undefined
+  }
+
 }
 
 function getNewestThread(){
   let threads = readThreads()
-  return threads[threads.length - 1].name
+  if (typeof threads !== "undefined"){
+    return threads[threads.length - 1].name
+  }
+  else{
+    return undefined
+  }
 }
 
 function threadExists(threadId){
   let threads = readThreads()
-  for (i = 0; i < threads.length; i++){
-    if (threadId == threads[i].name){
-      return true
-    } 
+  if (typeof threads !== "undefined"){
+    for (i = 0; i < threads.length; i++){
+      if (threadId == threads[i].name){
+        return true
+      }
+    }
   }
   return false
 }
